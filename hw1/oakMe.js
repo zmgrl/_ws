d3a0d70d81e7d709d6320a0e00da28e4f3351b8d
zmgrl <161786083+zmgrl@users.noreply.github.com>
2024-12-13 11:16:42 +0800

    Update oakMe.js

--- hw1/oakMe.js
+++ hw1/oakMe.js
@@ -1,1 +1,36 @@
+import { Application } from "https://deno.land/x/oak/mod.ts";
 
+const app = new Application();
+
+app.use((ctx) => {
+  // ctx.response.status = 404
+  console.log('url=', ctx.request.url)
+  let pathname = ctx.request.url.pathname
+  if (pathname == '/') {
+    ctx.response.body = `<html>
+<body>
+<h1>我的自我介紹</h1>
+<ol>
+<li><a href="/name">姓名</a></li>
+<li><a href="/age">年齡</a></li>
+<li><a href="/gender">性別</a></li>
+</ol>
+</body>
+</html>
+`
+  } else if (pathname == '/name') {
+    ctx.response.body = '林佩吟'
+  } else if (pathname == '/age') {
+    ctx.response.body = '19'
+  } else if (pathname == '/gender') {
+    ctx.response.body = '女' 
+  } else {
+    
+  } 
+    
+   
+  // ctx.response.body = 'Not Found!'
+});
+
+console.log('start at : http://127.0.0.1:8000')
+await app.listen({ port: 8000 })
