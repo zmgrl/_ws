4abd40699b048c1827d4c0a525d12b2cd81d0c62
zmgrl <161786083+zmgrl@users.noreply.github.com>
2024-12-13 20:44:37 +0800

    Create sign up.js

---
+++ 期中/sign up.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<style>
+body{background-color:#ECF5FF;}
+h1{text-align:center;}
+.centerDiv {
+  border: 3px outset white;  
+  text-align: center;
+}
+</style>
+<head>
+<title>Sign Up</title>
+</head>
+<body>
+
+  <h1>Sign up</h1>
+<Div class="centerDiv">
+  <form>
+    <label for="account">Account Name</label><br>
+    <input type="account" id="account" name="account"><br><br>
+    <label for="password">Password</label><br>
+    <input type="password" id="password" name="password"><br><br>
+        <label for="email">Email</label><br>
+    <input type="email" id="email" name="email" placeholder="ex:xxxx@gmail.com"/><br><br>
+    <label for="send out">Send out</label><br>
+    <input type="submit" id="send out" name="send out" value="send out"><br>
+  </form>
+  </Div>
+</body>
+</html>
